d0ecd422d8793feb737cffff090400b8aa86d107
Juhász Csenge <csenge799@gmail.com>
2023-05-11 00:19:49 +0200

    container folytatás

--- src/Page/Fooldal.tsx
+++ src/Page/Fooldal.tsx
@@ -4,22 +4,23 @@ import { Row, Col, Card, Container } from 'react-bootstrap';
 
 const Cards = () => {
     return (
-      <Container style={{padding:"40px", backgroundColor: "#E5F9DB"}}>
+      <Container style={{marginTop:"70px", backgroundColor: "#E5F9DB", borderRadius:"10px", padding: " 40px"}}>
         <Row>
           <Col lg='6' md='6'>
-            <div>
-              <Card.Img variant="top" src="/mazanyi-judit.jpg" alt='Judit' style={{width: "200px", height:"290px"}} />
+            <div style={{ display: 'flex', alignItems: 'center',  justifyContent: 'center'  }}>
+              <Card.Img variant="top" src="/mazanyi-judit.jpg" alt='Judit' className="border-0" style={{width: "250px", height:"300px", 
+              marginRight: "-5px", marginTop: "20px", marginBottom: "20px", borderRadius: "5px"}} />
             </div>
           </Col>
           <Col lg='6' md='6'>
               <div>
-                <Card.Title style={{padding: "5px"}}>Szakmai életút</Card.Title>
+                <Card.Title style={{padding: "5px", marginTop: "20px", marginBottom:"20px"}}>Szakmai életút</Card.Title>
                 <Card.Text>
                 Az ELTE Bölcsészkarán végeztem pszichológusként 1997-ben, 
                 majd 2008-ban szereztem meg a tanácsadó szakpszichológusi végzettséget krízis tanácsadás szakirányon. 
                 Főállásban pályaalkalmassági szakterületen dolgozom, itt szupervízori és szakértői feladatokat is ellátok. 
                 Emellett  tréning csoportokat vezetek és egyéni tanácsadást tartok.<br></br>
-                <Card.Title style={{padding: "10px"}}> További szakmai végzettségek</Card.Title>
+                <Card.Title style={{margin: "10px"}}> További szakmai végzettségek</Card.Title>
                 <ul>
                     <li>autogén tréning képzés</li>
                     <li>pszichoterapeuta képzés</li>
