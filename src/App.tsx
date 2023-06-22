05714a7e1ccb7bcce45a5df764fff971b32f2eea
Juhász Csenge <csenge799@gmail.com>
2023-06-22 23:42:28 +0200

    card elements

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 import { Routes, Route } from 'react-router';
 import 'bootstrap/dist/css/bootstrap.css'
-// import Fooldal from './Page/Fooldal';
 import Hero from './Page/Hero/Hero';
 import Header from './Component/Header/Header';
 import { Card } from 'react-bootstrap';
 import AboutCard from './Page/about/AboutCars';
+import Terapia from './Page/Terapia/Terapia';
 
 class App extends Component {
   render(): React.ReactNode {
@@ -15,6 +15,7 @@ class App extends Component {
                <Header/> 
                <Hero />
                <AboutCard/>
+               <Terapia />
                {/* <Fooldal/> */}
               {/* <Routes>
         <Route path='/'  element={<Fooldal></Fooldal>} />
--- src/Component/Data/Data.tsx
+++ src/Component/Data/Data.tsx
@@ -24,3 +24,48 @@ export const nav = [
       path: "/contact",
     },
   ]
+
+  export const terapia = [
+    {
+      list: "50",
+      Image: "/public/imam-fadly-UUACBQP62xw-unsplash.jpg",
+      address: "Liverpool, Canada",
+      name: "Sargam S. Singh",
+      // icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+    },
+    {
+      list: "70",
+      Image: "/public/pszichologus.jpg",
+      address: "Montreal, Canada",
+      name: "Harijeet M. Siller",
+      // icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+    },
+    {
+      list: "80",
+      Image: "/public/csoportterapia-1.jpg",
+      address: "Denever, USA",
+      name: "Anna K. Young",
+      // icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+    },
+    // {
+    //   list: "51",
+    //   cover: "../images/customer/team-4.jpg",
+    //   address: "2272 Briarwood Drive",
+    //   name: "Michael P. Grimaldo",
+    //   icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+    // },
+    // {
+    //   list: "42",
+    //   cover: "../images/customer/team-5.jpg",
+    //   address: "2272 Briarwood Drive",
+    //   name: "Michael P. Grimaldo",
+    //   icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+    // },
+    // {
+    //   list: "38",
+    //   cover: "../images/customer/team-5.jpg",
+    //   address: "Montreal, USA",
+    //   name: "Adam K. Jollio",
+    //   icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+    // },
+  ]
